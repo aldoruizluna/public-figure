fd6dea429098622da3556e8b1c48e5acff9a9d7c
Aldo Ruiz Luna <aldo.ruiz.luna@gmail.com>
2025-02-10 02:25:07 -0600

    Minor mod to next config js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  // Set the base path to your project’s GitHub Pages subdirectory
+  // Set the base path to your project's GitHub Pages subdirectory
   basePath: '/public-figure',
   // Ensure that asset references include the subdirectory
   assetPrefix: '/public-figure/',
   trailingSlash: true,
   output: 'export',
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
 };
