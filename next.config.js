3f828e10be212c378214fe5bf5d0e1dc2d2bfa79
Aldo Ruiz Luna <aldo.ruiz.luna@gmail.com>
2025-02-09 22:34:37 -0600

    Attempts to deploy to github pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+// next.config.js
 module.exports = {
-    i18n: {
-      locales: ['en', 'es'],
-      defaultLocale: 'en',
-    },
-  };
-  
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'es',
+  },
+  // Optional: if you need trailing slashes for GitHub Pages
+  trailingSlash: true,
+};
