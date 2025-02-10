12bd49ee03c26bec8ace44ed16fb781a2208eba2
Aldo Ruiz Luna <aldo.ruiz.luna@gmail.com>
2025-02-09 23:45:30 -0600

    Fixes js bug

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-// next.config.js
 module.exports = {
+  // Set the base path to your project’s GitHub Pages subdirectory
+  basePath: '/public-figure',
+  // Ensure that asset references include the subdirectory
+  assetPrefix: '/public-figure/',
   trailingSlash: true,
   output: 'export',
 };
