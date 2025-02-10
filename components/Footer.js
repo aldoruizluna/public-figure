ae5af59ff79cf473f6bd17b7c7c77a5ea0f7d1f4
Aldo Ruiz Luna <aldo.ruiz.luna@gmail.com>
2025-02-09 23:51:27 -0600

    Minor copy fix

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <footer className="footer">
         <div className="container">
-          <p>&copy; {new Date().getFullYear()} {`Al Do Ruiz Luna`}. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} {`Aldo Ruiz Luna`}. All rights reserved.</p>
         </div>
       </footer>
     );
